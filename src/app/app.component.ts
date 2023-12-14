908ee023cb488c63a165d0512cb5d950c72bc686
omaramsoul <o.amsoul@gmail.com>
2023-12-14 16:55:34 +0100

    Set active link button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,10 +10,17 @@ import { RouterLink, RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  
   title = 'e-shop';
+  currentComponent: string = '';
+
   components: Array<{title: string, route: string}> = [
     {title: 'Home', route: '/home'},
     {title: 'New product', route: '/new-product'},
     {title: 'Products', route: '/products'}
   ]
+
+  setCurrentComponent(component: string) {
+    this.currentComponent = component;
+  }
 }
