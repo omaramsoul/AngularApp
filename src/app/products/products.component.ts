8c8936a3c27165af18e4b50fd62076032afee3d4
omaramsoul <o.amsoul@gmail.com>
2023-12-14 17:58:58 +0100

    Adding the product table

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -8,5 +8,9 @@ import { Component } from '@angular/core';
   styleUrl: './products.component.css'
 })
 export class ProductsComponent {
-
+  products: Array<{id: number, title: string, price: number, checked: boolean}> = [
+    {id: 1, title: 'Iphone 14 pro', price: 1100, checked: true},
+    {id: 2, title: 'Iphone 11', price: 600, checked: false},
+    {id: 3, title: 'Iphone 13 normal', price: 700, checked: true}
+  ];
 }
