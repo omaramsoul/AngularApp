e8a942047630dc1d275f19479ea00b8824f3b709
omaramsoul <o.amsoul@gmail.com>
2023-12-14 16:23:15 +0100

    Home, Products, New product components with the routing

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink, RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, RouterLink],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'e-shop';
+  components: Array<{title: string, route: string}> = [
+    {title: 'Home', route: '/home'},
+    {title: 'New product', route: '/new-product'},
+    {title: 'Products', route: '/products'}
+  ]
+}
