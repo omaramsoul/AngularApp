49074108520bf37d4384417124abf7af4b53a546
omaramsoul <o.amsoul@gmail.com>
2023-12-20 14:43:08 +0100

    Adding json server api for front testing purpose

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink],
+  imports: [CommonModule, RouterOutlet, RouterLink, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-products',
@@ -7,10 +8,27 @@ import { Component } from '@angular/core';
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
-export class ProductsComponent {
-  products: Array<{id: number, title: string, price: number, checked: boolean}> = [
-    {id: 1, title: 'Iphone 14 pro', price: 1100, checked: true},
-    {id: 2, title: 'Iphone 11', price: 600, checked: false},
-    {id: 3, title: 'Iphone 13 normal', price: 700, checked: true}
-  ];
+export class ProductsComponent implements OnInit{
+
+  products : Array<any> = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http.get<any>("http://localhost:8089/products").subscribe(
+      {
+        next: data => this.products = data,
+        error: err => console.log(err)
+      });
+  }
+
+  // products: Array<{id: number, title: string, price: number, checked: boolean}> = [
+  //   {id: 1, title: 'Iphone 14 pro', price: 1100, checked: true},
+  //   {id: 2, title: 'Iphone 11', price: 600, checked: false},
+  //   {id: 3, title: 'Iphone 13 normal', price: 700, checked: true}
+  // ];
+
+  handleCheckProduct(product: {id: number, title: string, price: number, checked: boolean}) {
+    product.checked = !product.checked;
+  }
 }
